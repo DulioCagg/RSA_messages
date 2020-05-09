a24e5ba578d211bb967b6bba6a1a7d94195de073
DulioCagg <duliop.caggiano@hotmail.com>
2020-05-08 19:47:55 -0500

    Added login

--- main.js
+++ main.js
@@ -1,46 +1,39 @@
 const express = require("express");
 const RSA = require("./script");
 
-let login = "";
+let currentUser = "";
 let users = {};
 let messages = {};
 
 const temp = ["Oscar", "Dulio", "Diana"];
-
 const generateKeys = (name) => {
-   let size = RSA.toKey(name);
-   let keys = RSA.generate(size);
-   const user = {
-      n: keys.n,
-      d: keys.d,
-      e: keys.e,
-   };
-   users[name] = user;
+   const lower = name.toLowerCase();
+   const size = RSA.toKey(lower);
+   const keys = RSA.generate(size);
+   users[name] = keys;
 }
 
-temp.map(user => {
-   generateKeys(user);
-})
-
+temp.forEach(user => generateKeys(user))
 
 const app = express();
-
 app.use(express.json());
-
-app.use("/", express.static("./interface/"));
-
-app.get("/", (req, res) => {
-
+app.use((req, res, next) => {
+   console.log(req.method);
+   console.log(req.url);
+   console.log(req.body);
+   next();
 });
 
+app.use(express.static("interface"));
+
 app.get("/users", (req, res) => {
-   const list = Object.keys(users).filter(user => user != login);
+   const list = Object.keys(users).filter(user => user != currentUser);
 
    res.json({ users: list }).status(200);
 });
 
 app.get("/messages", (req, res) => {
-   res.json(messages[login]).status(200);
+   res.json(messages[currentUser]).status(200);
 });
 
 app.post("/send", (req, res) => {
@@ -54,7 +47,7 @@ app.post("/send", (req, res) => {
    const decoded_message = RSA.decode(decrypted_message);
    const messageToAdd = {
       message,
-      from: login,
+      from: currentUser,
       encrypted: encrypted_message,
       decrypted: decrypted_message
    }
@@ -66,13 +59,15 @@ app.post("/send", (req, res) => {
 app.post("/register", (req, res) => {
    const reqUser = req.body.username;
 
-   if (users[reqUser] == typeof "undefined") {
-      generateKeys(reqUser)
+   currentUser = reqUser;
+   console.log("The user loged in is: ", currentUser);
+   if (typeof users[reqUser] === "undefined") {
+      generateKeys(reqUser);
       messages[reqUser] = [];
+      res.json({}).status(200);
+   } else {
+      res.json({}).status(200);
    }
-
-   login = reqUser;
-   res.json().status(200);
 });
 
 app.listen(3000, () => console.log("Listening in port 3000"));
